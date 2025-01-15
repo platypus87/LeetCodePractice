221f2b034480e517511c55009fe080dada250516
Matthew White <amnwhitejr@gmail.com>
2025-01-15 17:31:05 -0500

    Attempt at LeetCode's 'IsPalindrome' problem. Not finished yet

---
+++ IsPalindrome/IsPalindrome/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine(IsPali(121));
+bool IsPali(int testNumber)
+{
+    string testString = testNumber.ToString();
+    char[] reverseString = testString.ToCharArray();
+    string finalString = reverseString.ToString();
+
+    Console.WriteLine($"TestString is: {testString}");
+    Console.WriteLine($"reverseString is: {reverseString.ToString()}");
+    Console.WriteLine($"FinalString is: {finalString}");
+    foreach(char c in reverseString) {  Console.WriteLine(c); }
+
+    if (testString == finalString) {  return true; }
+    else
+    return false;
+}
